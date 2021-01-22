688e60283d4e57b4cf287cc22733832985cf5d93
name <2963703433@qq.com>
2021-01-22 18:15:47 +0800

    界面构建完成：登录页面

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+//登录路由配置
+router.get('/login',function(req,res){
+  res.render('login',{})
+})
+//文章详情页路由配置
+router.get('/article',function(req,res){
+  res.render('details',{})
+})
 
 module.exports = router;
